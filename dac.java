3ff2558fe33cd19e4add3aa1dd621921341ca82a
Erik Öhrn <erik.ohrn@outlook.com>
2016-10-31 11:20:54 +0100

    Binary search

--- dac.java
+++ dac.java
@@ -21,6 +21,38 @@ public class dac{
     return binarySearch(Arrays.copyOfRange(sortedArray,middle+1,sortedArray.length),x,pos+middle+1);
   }
 
+
+  //Merge sort
+  public static int[] mergeSort(int[] unsorted){
+      int pivot = Math.floor(Math.random()*unsorted.length);
+      return merge(mergeSort(Arrays.copyOfRange(unsorted,0,pivot)), mergeSort(Arrays.copyOfRange(unsorted,pivot,unsorted.length)));
+  }
+
+  public static int[] merge(int[] a, int[] b){
+    if(a.length == 0)
+      return b;
+    if(b.length == 0)
+      return a;
+
+      int[] res = new int[a.length+b.length];
+      int o,i,j = 0;
+      while(o<res.length){
+        if(j >= b.length || a[i]<b[j] ){
+          res[o]=a[i]
+          i++;
+        } else {
+          res[o]=b[j]
+          j++;
+        }
+        o++;
+    }
+    return res;
+
+
+
+  }
+
+
   public static void main(String[] args){
     System.out.println("Hitta 55:s position i [-4,3,4,8,12,54,55,56,122,123,124,125,177,777,878]");
     int[] arrayen = {-4,3,4,8,12,54,55,56,122,123,124,125,177,777,878};
@@ -29,6 +61,9 @@ public class dac{
     System.out.println("\nHitta 500:s position i [-4,3,4,8,12,54,55,56,122,123,124,125,177,777,878]");
     System.out.println(binarySearch(arrayen, 500));
 
+    System.out.println("\nSortera [54,2,7,9,445,8,9,34,-54,-56,-5,0,1231]");
+    int[] mergeArray = {54,2,7,9,445,8,9,34,-54,-56,-5,0,1231};
+    System.out.println(mergeSort(mergeArray));
 
   }
 
